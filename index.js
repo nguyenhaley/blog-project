cbe4445fa2cbcbd830d8d2dbcff85a0f2176b978
nguyenhaley <hhn007@ucsd.edu>
2025-12-15 15:52:09 -0800

    pc update

--- index.js
+++ index.js
@@ -8,15 +8,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+// tells Express where static files are (for css)
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("welcomePage.ejs", {
+  res.render("welcomePage.ejs", {});
+});
 
-    });
+app.get("/homepage", (req, res) => {
+  res.render("homepage.ejs", {});
+});
+
+app.get("/create", (req, res) => {
+  res.render("blogCreationPage.ejs", {});
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
